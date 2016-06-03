7014aaf920cf7cd365d3056b05c291388c5eee51
giorgi khundzakishvili <alowap@mail.ru>
2016-06-03 20:55:08 +0000

    auth.js deleted online with Bitbucket

--- config/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    'facebookAuth': {
-        'clientID': '1692724934314317', // your App ID
-        'clientSecret': '779652f5175235c2df8cdec0b3f6bba2', // your App Secret
-        'callbackURL': 'http://localhost:1337/connect/facebook/callback/'
-    }
-};
