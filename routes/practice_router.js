84c1a7e530d355b3c37f65a60068961f7a66832d
Khundzaka <khundzaka@gmail.com>
2016-11-18 00:26:59 +0400

    increase version number, add practice result and stats

---
+++ routes/practice_router.js
@@ -0,0 +1,21 @@
+var Router = require("express").Router;
+var WordRepository = require("../app/word_repository").WordRepository;
+//var requiresPermission = require("../app/middleware").requiresPermission;
+var wordRouter = Router();
+
+var defaultFailResponse = {status: "failed"};
+
+// todo: router needs auth
+
+wordRouter.post("/find", function (req, res) {
+    WordRepository.find({value: req.body.value, accepted: true}, function (err, words) {
+        if (err) {
+            console.log(err);
+            return res.json(defaultFailResponse);
+        }
+
+        return res.json({status: "ok", data: {words: words}});
+    });
+});
+
+module.exports = wordRouter;
